f51bcb2d17ea3ee622de4a6e9e3bb316a456a02d
jonatan360 <japarrat@unal.edu.co>
2018-01-26 23:58:12 -0500

    Se agrega libreria para mostrar notificaciones

---
+++ public/js/configuracion/notificaciones.js
@@ -0,0 +1,23 @@
+// http://codeseven.github.io/toastr/demo.html
+function llamar_notificacion(){
+
+  Command: toastr["success"]("contenido", "titulo")
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-bottom-full-width",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+}
